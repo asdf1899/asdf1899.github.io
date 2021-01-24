3702ca2aa7049c31a1b6160efd3b5540b748342a
asdf1899 <araid.anas99@gmail.com>
2021-01-24 18:31:48 +0100

    Add home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FullpageSection } from '@ap.cx/react-fullpage'
 //import Navbar from './Navbar';
+import Typist from 'react-typist';
+import TypistLoop from 'react-typist-loop';
 
-const Home = () => (
-  <FullpageSection>
-    <section className="flex bg-whit h-screen">
-      <div className="flex w-1/2 text-center justify-center align-middle items-center">
-        <div>
-          <h1 className="text-gray-800 justify-center align-middle">Hi!</h1>
+const Home = () => {
+  const [text] = useState('an UX designer');
+  /*const changeText = () => {
+    setText('a developer');
+    console.log(text)
+  }*/
+  console.log(text)
+  return (
+    <FullpageSection>
+      <section className="flex h-screen">
+        <div className="flex flex-col w-full text-left justify-center items-center px-12 lg:px-48 sm:px-8 md:px-24">
+          <h4 className="text-gray-800 text-3xl sm:text-2xl md:text-5xl lg:text-5xl xl:text-6xl w-full style-home-text">Hi!<span className="wave">👋</span></h4>
+          <h4 className="text-gray-800 text-3xl md:text-6xl lg:text-6xl xl:text-6xl w-full style-home-text">I&apos;m Anas,</h4>
+          <TypistLoop style="text-left w-full" interval={1000}>
+            {[
+              'an UX designer',
+              'a developer',
+              'a firefighter',
+            ].map(text => <Typist className="text-left w-full text-gray-800 text-3xl md:text-6xl lg:text-6xl xl:text-6xl style-home-text" key={text} startDelay={500}>
+              {text}
+              <Typist.Backspace count={text.length} delay={1000} />
+            </Typist>)}
+          </TypistLoop>
+          <h4 className="text-gray-800 text-3xl md:text-6xl lg:text-6xl xl:text-6xl w-full style-home-text">
+            based in <a onClick={() => window.open("https://goo.gl/maps/6fnYAANFEEsi1Mce7", "_blank")} className="style-gradient-text cursor-pointer">Trento</a>⛰️
+          </h4>
         </div>
-      </div>
-
-      <div className="flex w-1/2 text-right justify-end">
-
-      </div>
-    </section>
-  </FullpageSection>
-)
+      </section>
+    </FullpageSection>
+  )
+}
 
 export default Home;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 import React from 'react';
 const Navbar = () => (
-  <header className="p-6 flex h-10 bg-white w-full" style={{position: 'fixed',zIndex:'1000',top: '0'}}>
+  <header className="p-6 flex h-10 w-full" style={{position: 'fixed',zIndex:'1000',top: '0', backgroundColor: '#fafafa'}}>
     <div className="w-1/3 text-center">
-      <h3 className="text-gray-800">Anas Araid</h3>
+      <h3 className="text-gray-800 cursor-pointer" onClick={() => window.scrollTo(0,0)}>Anas Araid</h3>
     </div>
     <div className="w-1/3 text-center p-2">
       <h2 className="text-lg text-gray-700"></h2>
