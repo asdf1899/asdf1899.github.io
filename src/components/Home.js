9270fa32b4ad7fbd1fb551eb0f43db69db9f7dc2
asdf1899 <araid.anas99@gmail.com>
2021-02-08 21:14:44 +0100

    Add arrow animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ import { Blob } from 'react-blob'
 
 const Home = () => (
   <FullpageSection>
-    <section className="flex h-screen">
+    <section className="flex h-3/4">
       <BackgroundBlob size="90vh" />
       <BackgroundBlob size="80vh" />
       <div className="flex flex-col w-full text-left justify-center items-center px-12 lg:px-48 sm:px-8 md:px-24">
@@ -32,6 +32,14 @@ const Home = () => (
         <h4 className="style-text-white text-3xl md:text-6xl lg:text-6xl xl:text-6xl w-full style-home-text">
           based in <a onClick={() => window.open("https://goo.gl/maps/6fnYAANFEEsi1Mce7", "_blank")} className="style-gradient-text cursor-pointer">Trento</a>⛰️
         </h4>
+      </div>
+     
+    </section>
+    <section className="flex h-1/4 flex-col w-full text-center align-bottom">
+      <div className="text-center">
+        <svg className="h-10 w-full arrow-animation" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white">
+          <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+        </svg>
       </div>
     </section>
   </FullpageSection>
